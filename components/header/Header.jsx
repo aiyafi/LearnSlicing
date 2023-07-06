66bb9e5c58eec52d3b013cedb323b6ceb5fe8fba
aiyafi <afrizalyafi@gmail.com>
2023-07-06 16:37:32 +0700

    update 2.0

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import "./header.css"
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Logo from "/src/assets/logo.svg"
+import React from 'react';
+import "./header.css";
+import Logo from "/src/assets/logo.svg";
 
 const Header = () => {
     return (
         <div>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <img src={Logo} alt="ClarityUILogo" />
-                    </Navbar.Brand>
-                    <Nav.Link href="#home" className="nav-link">Products</Nav.Link>
-                    <Nav.Link href="#link" className="nav-link">Features</Nav.Link>
-                    <Nav.Link href="#link" className="nav-link">Pricing</Nav.Link>
-                    <Nav.Link href="#link" className="nav-link">Support</Nav.Link>
-                    <button type="button" className="btn btn-outline-dark">Start free trial</button>
-                </Container>
-            </Navbar>
+            <header>
+                <img className="logo" src={Logo} alt="ClarityUILogo" />
+                <nav className="nav-bar">
+                    <a className="nav-bar-text" href="#products">Products</a>
+                    <a className="nav-bar-text" href="#features">Features</a>
+                    <a className="nav-bar-text" href="#pricing">Pricing</a>
+                    <a className="nav-bar-text" href="#support">Support</a>
+                </nav>
+                <button className="button-register">Start free trial</button>
+            </header>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
